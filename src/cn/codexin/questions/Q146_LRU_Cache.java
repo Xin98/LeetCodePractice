9e9f0b5de714275345444c7ec391be8e64f49347
Xin98 <emgaox@163.com>
2020-04-01 13:16:42 +0800

    Q146修改

--- src/cn/codexin/questions/Q146_LRU_Cache.java
+++ src/cn/codexin/questions/Q146_LRU_Cache.java
@@ -10,10 +10,8 @@ import java.util.Map;
 public class Q146_LRU_Cache {
 
     private Map<Integer, Integer> data;
-    private int capacity;
     public Q146_LRU_Cache(int capacity) {
-        this.capacity = (int)(capacity / 0.75f);
-        data = new LinkedHashMap<Integer, Integer>(capacity, 0.75f, true){
+        data = new LinkedHashMap<Integer, Integer>((int)(capacity / 0.75f), 0.75f, true){
             @Override
             protected boolean removeEldestEntry(Map.Entry eldest) {
                 if(data.size() > capacity) return true;
@@ -29,10 +27,12 @@ public class Q146_LRU_Cache {
         data.put(key, value);
     }
     public static void main(String[] args) {
-        Q146_LRU_Cache q146_lru_cache = new Q146_LRU_Cache(2);
+        Q146_LRU_Cache q146_lru_cache = new Q146_LRU_Cache(3);
         q146_lru_cache.put(1,1);
         q146_lru_cache.put(2,2);
+        q146_lru_cache.put(4,2);
+        q146_lru_cache.put(6,2);
         q146_lru_cache.put(3,3);
-        System.out.println(q146_lru_cache.get(2));
+        System.out.println(q146_lru_cache.get(4));
     }
 }
