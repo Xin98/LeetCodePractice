58955e6ad69c9b0fea9669e1357764a4c294c1e2
499689361 <499689361@qq.com>
2020-04-02 18:26:39 +0800

    20200402打卡1题

---
+++ src/cn/codexin/questions/two-sum.java
@@ -0,0 +1,31 @@
+/**
+给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+
+你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+
+示例:
+
+给定 nums = [2, 7, 11, 15], target = 9
+
+因为 nums[0] + nums[1] = 2 + 7 = 9
+所以返回 [0, 1]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/two-sum
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。 */
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] indexs = new int[2];
+        HashMap<Integer,Integer> hash = new HashMap<Integer,Integer>();
+        for(int i = 0; i < nums.length; i++){
+            if(hash.containsKey(nums[i])){
+                indexs[0] = i;
+                indexs[1] = hash.get(nums[i]);
+                return indexs;
+            }
+            hash.put(target-nums[i],i);
+        }
+
+        return indexs;
+    }
+}
