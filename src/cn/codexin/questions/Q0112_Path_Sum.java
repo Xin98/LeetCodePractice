b1ca16b2a9b8586c2f88dd7c9ad6617d6fbe9339
Xin98 <emgaox@163.com>
2020-07-07 08:21:54 +0800

    2020-07-07打卡1题

---
+++ src/cn/codexin/questions/Q0112_Path_Sum.java
@@ -0,0 +1,19 @@
+package cn.codexin.questions;
+
+/**
+ * Created by xinGao 2020/7/7
+ */
+
+public class Q0112_Path_Sum {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null) {
+            return false;
+        }
+        if(root.left == null && root.right == null) {
+            return sum == root.val;
+        }
+        return hasPathSum(root.left, sum - root.val) ||
+                hasPathSum(root.right, sum - root.val);
+    }
+
+}
