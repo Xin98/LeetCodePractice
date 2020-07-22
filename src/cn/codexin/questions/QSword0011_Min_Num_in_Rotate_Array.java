b0c768c39375a73d11fa533029b8d4a34fd4dd12
Xin98 <emgaox@163.com>
2020-07-22 08:51:00 +0800

    2020-07-22打卡1题

--- src/cn/codexin/questions/QSword0011_Min_Num_in_Rotate_Array.java
+++ src/cn/codexin/questions/QSword0011_Min_Num_in_Rotate_Array.java
@@ -5,4 +5,19 @@ package cn.codexin.questions;
  */
 
 public class QSword0011_Min_Num_in_Rotate_Array {
+    public int minArray(int[] numbers) {
+        int lo = 0;
+        int hi = numbers.length - 1;
+        while(lo < hi) {
+            int mid = (hi - lo) / 2 + lo;
+            if(numbers[mid] > numbers[hi]) {
+                lo = mid;
+            } else if(numbers[mid] < numbers[hi]){
+                hi = mid;
+            } else {
+                hi--;
+            }
+        }
+        return numbers[lo];
+    }
 }
