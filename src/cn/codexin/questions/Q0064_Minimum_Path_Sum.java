1a488ba85c914e8d29e00316c43c085b07a6a04f
Xin98 <emgaox@163.com>
2020-07-23 08:48:32 +0800

    2020-07-23打卡1题

---
+++ src/cn/codexin/questions/Q0064_Minimum_Path_Sum.java
@@ -0,0 +1,30 @@
+package cn.codexin.questions;
+
+/**
+ * Created by xinGao 2020/7/23
+ */
+
+public class Q0064_Minimum_Path_Sum {
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        if(m == 0) {
+            return 0;
+        }
+        int n = grid[0].length;
+        int[] dp = new int[n];
+        dp[0] = grid[0][0];
+        for (int i = 1; i < n; i++) {
+            dp[i] = dp[i-1] + grid[0][i];
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if(j == 0) {
+                    dp[j] = dp[j] + grid[i][j];
+                } else {
+                    dp[j] = Math.min(dp[j], dp[j-1]) + grid[i][j];
+                }
+            }
+        }
+        return dp[n-1];
+    }
+}
