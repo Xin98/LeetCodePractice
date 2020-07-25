d3d1d3489b2b63f0297650f3f4d84428fe0483a2
Xin98 <emgaox@163.com>
2020-07-25 09:01:47 +0800

    2020-07-24打卡1题

---
+++ src/cn/codexin/questions/Q1025_Divisor_Game.java
@@ -0,0 +1,11 @@
+package cn.codexin.questions;
+
+/**
+ * Created by xinGao 2020/7/24
+ */
+
+public class Q1025_Divisor_Game {
+    public boolean divisorGame(int N) {
+        return N % 2 == 0;
+    }
+}
