b1c932cf45b381fd554ba56649418b50f28a8a83
Xin98 <emgaox@163.com>
2020-08-07 08:57:56 +0800

    2020-08-07打卡1题

---
+++ src/cn/codexin/questions/Q0100_Same_Tree.java
@@ -0,0 +1,17 @@
+package cn.codexin.questions;
+
+/**
+ * Created by xinGao 2020/8/7
+ */
+
+public class Q0100_Same_Tree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null) {
+            return true;
+        }
+        if(p == null || q == null) {
+            return false;
+        }
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
