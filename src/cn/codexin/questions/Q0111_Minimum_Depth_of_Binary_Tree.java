833c48b1f0ede1d7bc95dc283275297621fbaaf3
Xin98 <emgaox@163.com>
2020-08-21 08:31:44 +0800

    2020-08-21打卡1题

---
+++ src/cn/codexin/questions/Q0111_Minimum_Depth_of_Binary_Tree.java
@@ -0,0 +1,20 @@
+package cn.codexin.questions;
+
+/**
+ * Created by xinGao 2020/8/21
+ */
+
+public class Q0111_Minimum_Depth_of_Binary_Tree {
+    public int minDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        if(root.left == null && root.right == null) {
+            return 1;
+        }
+        if(root.left == null || root.right == null) {
+            return Math.max(minDepth(root.left), minDepth(root.right)) + 1;
+        }
+        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    }
+}
