4fca982b3526fcad8f1c3044f55c077166d601d6
Xin98 <emgaox@163.com>
2021-01-14 13:19:33 +0800

    2021-01-13打卡1题

---
+++ src/cn/codexin/questions/Q1018_Binary_Prefix_Divisible_By_5.java
@@ -0,0 +1,25 @@
+package cn.codexin.questions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by xinGao 2021/1/14
+ */
+
+public class Q1018_Binary_Prefix_Divisible_By_5 {
+    public List<Boolean> prefixesDivBy5(int[] A) {
+        int n = A.length;
+        int acc = 0;
+        List<Boolean> ans = new ArrayList<>(n);
+        for(int num : A) {
+            acc = ((acc << 1) + num) % 5;
+            if(acc == 0) {
+                ans.add(true);
+            } else {
+                ans.add(false);
+            }
+        }
+        return ans;
+    }
+}
