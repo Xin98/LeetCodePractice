07001773532923a057f5591511f3d945a8cda599
Xin98 <emgaox@163.com>
2021-02-25 19:08:34 +0800

    2021-02-25打卡1题

---
+++ src/cn/codexin/questions/Q0867_Transpose_Matrix.java
@@ -0,0 +1,19 @@
+package cn.codexin.questions;
+
+/**
+ * Created by xinGao 2021/2/25
+ */
+
+public class Q0867_Transpose_Matrix {
+    public int[][] transpose(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] ans = new int[n][m];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                ans[j][i] = matrix[i][j];
+            }
+        }
+        return ans;
+    }
+}
