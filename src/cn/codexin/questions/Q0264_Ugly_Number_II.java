c01ec4f19db33243df1a94e1bad615475feede86
Xin98 <emgaox@163.com>
2021-04-11 13:36:30 +0800

    2021-04-11打卡1题

---
+++ src/cn/codexin/questions/Q0264_Ugly_Number_II.java
@@ -0,0 +1,27 @@
+package cn.codexin.questions;
+
+/**
+ * Created by xinGao 2021/4/11
+ */
+
+public class Q0264_Ugly_Number_II {
+    public int nthUglyNumber(int n) {
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        int p2 = 1, p3 = 1, p5 = 1;
+        for(int i = 2; i <= n; i++) {
+            int num2 = dp[p2] * 2, num3 = dp[p3] * 3, num5 = dp[p5] * 5;
+            dp[i] = Math.min(num2, Math.min(num3, num5));
+            if(dp[i] == num2) {
+                p2++;
+            }
+            if(dp[i] == num3) {
+                p3++;
+            }
+            if(dp[i] == num5) {
+                p5++;
+            }
+        }
+        return dp[n];
+    }
+}
