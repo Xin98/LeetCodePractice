217217e85e547b32481b575d8b78c82ad14327f3
Xin98 <emgaox@163.com>
2021-06-11 21:40:16 +0800

    2021-06-11打卡1题

---
+++ src/cn/codexin/questions/Q0279_Perfect_Squares.java
@@ -0,0 +1,19 @@
+package cn.codexin.questions;
+
+/**
+ * Created by xinGao 2021/6/11
+ */
+
+public class Q0279_Perfect_Squares {
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        for(int i = 1; i <= n; i++) {
+            int min = Integer.MAX_VALUE;
+            for(int j = 1; j * j <= i; j++) {
+                min = Math.min(min, dp[i - j * j]);
+            }
+            dp[i] = min + 1;
+        }
+        return dp[n];
+    }
+}
