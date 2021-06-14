570202d64ce4639b3c50d54cc3deaf09a24d6527
Xin98 <emgaox@163.com>
2021-06-14 08:50:08 +0800

    2021-06-13打卡1题

---
+++ src/cn/codexin/questions/Q0278_First_Bad_Version.java
@@ -0,0 +1,25 @@
+package cn.codexin.questions;
+
+/**
+ * Created by xinGao 2021/6/13
+ */
+
+public class Q0278_First_Bad_Version {
+    public int firstBadVersion(int n) {
+        int left = 1, right = n;
+        while (left < right) { // 循环直至区间左右端点相同
+            int mid = left + (right - left) / 2; // 防止计算时溢出
+            if (isBadVersion(mid)) {
+                right = mid; // 答案在区间 [left, mid] 中
+            } else {
+                left = mid + 1; // 答案在区间 [mid+1, right] 中
+            }
+        }
+        // 此时有 left == right，区间缩为一个点，即为答案
+        return left;
+    }
+    // example
+    boolean isBadVersion(int version){
+        return true;
+    }
+}
